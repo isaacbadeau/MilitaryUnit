47585d2dd38bb5f81d4160c25ed86919208f8023
Isaac Badeau <isaacbadeau@gmail.com>
2019-04-05 08:08:44 -0400

    Final

--- Program.cs
+++ Program.cs
@@ -16,14 +16,21 @@ namespace MilitaryUnit
         {
             Random rndDmg = new Random();
             List<Vehicle> gameVehicles = new List<Vehicle> { new Tank(), new TranspoTruck(), new Helicopter() };
-            List<Weapon> gameWeapons = new List<Weapon> { new SubMachineGun(), new SAW(), new SniperRifle() };
+            List<Weapon> gameWeapons = new List<Weapon> { new MachineGun(), new SAW(), new SniperRifle() };
             List<Soldier> gameSoldiers = new List<Soldier> { new FootSoldier(), new HeavySoldier(), new Sniper() };
 
             gameVehicles[2].loadTroops(8);
+            Console.WriteLine();
             gameVehicles[2].disembarkTroops(8);
+            Console.WriteLine();
             gameSoldiers[0].Move(3, 5);
+            Console.WriteLine();
             gameSoldiers[0].TakesDamage(rndDmg.Next(0,20));
+            Console.WriteLine();
             gameWeapons[0].FireWeapon();
+            Console.WriteLine();
+            gameWeapons[1].FireWeapon();
+            Console.WriteLine();
 
         }
     }
